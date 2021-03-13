744e0188ce87aa6e8208eb319a7ed453516de6f1
carlosiglesias1 <carlosig490@gmail.com>
2021-03-14 00:50:23 +0100

    "Commit Sumador"

--- src/main/java/github/Sumador.java
+++ src/main/java/github/Sumador.java
@@ -1,17 +1,16 @@
-package github;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package github;
+
 /**
  *
  * @author carlo
  */
 public class Sumador {
-
-    public static float sumar(float a, float b) {
-        return a + b;
+    public static float sumar (float num, float num2){
+        return num+num2;
     }
 }
