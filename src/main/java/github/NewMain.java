12a828f5b3e3e86990052dc508eb8f326d7dbb0e
carlosiglesias1 <carlosig490@gmail.com>
2021-03-14 01:02:18 +0100

    "Commit Sumador hotfix"

--- src/main/java/github/NewMain.java
+++ src/main/java/github/NewMain.java
@@ -12,6 +12,6 @@ package github;
 public class NewMain {
 
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar(15f, 4f));
+        System.out.println(Sumador.sumar(16f, 4f));
     }
 }
